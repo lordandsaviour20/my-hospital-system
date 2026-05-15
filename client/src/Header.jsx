24430036d6bdd35476d8ffcf4082edd01e670c06
Aneega S. Panagoda <isaic.sierra16@gmail.com>
2026-05-15 22:03:48 +0530

    Work in progress on header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -19,7 +19,7 @@ function Header(){
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/booking">Booking</Link></li>
-                    <li><Link to="/contactUs">Contact</Link></li>
+                    <li><Link to="/contactus">Contact</Link></li>
                     <li><Link to="/services">Services</Link></li>
                 </ul> 
             </nav>
